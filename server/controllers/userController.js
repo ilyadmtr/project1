687370cd556f2e50b471eada64c8cc039f67d4e8
Igor Lialko <igorlialko8@gmail.com>
2022-03-02 15:45:06 +0200

    auth

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,6 @@
 const ApiError=require('../error/ApiError')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 const {User,Basket}=require('../models/models')
 
 class UserController{
@@ -9,7 +10,13 @@ class UserController{
         if(!email || !password){
             return next(ApiError.badRequest('Некорректный email или password'))
         }
-
+        const candidate = await User.findOne({where:{email}})
+        if(candidate){
+            return next(ApiError.badRequest('Пользователь с таким email уже существует'))
+        }
+        const hashPassword = await bcrypt.hash(password,5)
+        const user = await  User.create({email,role,password:hashPassword})
+        const basket = await  Basket.create({userId:user.id})
     }
     async login(req,res){
 
